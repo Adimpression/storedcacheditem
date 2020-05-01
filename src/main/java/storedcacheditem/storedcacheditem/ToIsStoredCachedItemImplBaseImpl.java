0e3479c060e2f23545c6757b75d453e275c0098c
Ravindranath Akila <ravindranathakila@gmail.com>
2020-05-01 22:31:06 +0800

    Working

--- src/main/java/storedcacheditem/storedcacheditem/ToIsStoredCachedItemImplBaseImpl.java
+++ src/main/java/storedcacheditem/storedcacheditem/ToIsStoredCachedItemImplBaseImpl.java
@@ -1,18 +1,42 @@
 package storedcacheditem.storedcacheditem;
 
+import com.google.protobuf.ByteString;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ILock;
+import id.id.IsId;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
+import storedcacheditem.input.IsInput;
+import storedcacheditem.output.IsOutput;
 
+import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ToIsStoredCachedItemImplBaseImpl extends ToIsStoredCachedItemGrpc.ToIsStoredCachedItemImplBase {
 
     private final Logger logger;
+    private final HazelcastInstance hzInstance;
+    private final Map<String, ByteString> general;
+    private final ILock lock;
 
     public ToIsStoredCachedItemImplBaseImpl() {
         logger = Logger.getLogger(getClass().getName());
         logger.info("starting");
+
+        logger.info("starting hazelcast");
+        hzInstance = Hazelcast.newHazelcastInstance();
+        logger.info("started hazelcast");
+
+        logger.info("starting general map: hazelcast");
+        general = hzInstance.getMap("general");
+        logger.info("started general map: hazelcast");
+
+        logger.info("starting general lock: hazelcast");
+        lock = hzInstance.getLock("general");
+        logger.info("started general lock: hazelcast");
 
         logger.info("started");
     }
@@ -20,13 +44,51 @@ public class ToIsStoredCachedItemImplBaseImpl extends ToIsStoredCachedItemGrpc.T
     @Override
     public void produce(final NotStoredCachedItem request, final StreamObserver<IsStoredCachedItem> responseObserver) {
         try {
+            final IsId isId;
             if (!request.hasIsInput()) {
                 throw new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription("422"));
             }
+            final IsInput isInput = request.getIsInput();
+            if (!isInput.hasIsId()) {
+                throw new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription("422"));
+            } else {
+                isId = isInput.getIsId();
+                if (!isId.hasIsOutput()) {
+                    throw new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription("422"));
+                }
+            }
+            if (isInput.getIsItemBytes() == null) {
+                throw new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription("422"));
+            }
+            if (isInput.getIsItemBytes()
+                    .isEmpty()) {
+                throw new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription("422"));
+            }
+
+            if (isInput.getIsUseLockBoolean()) {
+                lock.lock();
+                try {
+                    general.put(isId.getIsOutput()
+                                    .getIsStringValue(),
+                            isInput.getIsItemBytes());
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                general.put(isId.getIsOutput()
+                                .getIsStringValue(),
+                        isInput.getIsItemBytes());
+            }
+
             responseObserver.onNext(IsStoredCachedItem.newBuilder()
+                    .setIsOutput(IsOutput.newBuilder()
+                            .build())
                     .build());
             responseObserver.onCompleted();
         } catch (final Exception e) {
+            logger.log(Level.SEVERE,
+                    e.getMessage(),
+                    e);
             responseObserver.onError(e);
         }
     }
--- src/test/java/storedcacheditem/storedcacheditem/ToIsStoredCachedItemImplBaseImplTest.java
+++ src/test/java/storedcacheditem/storedcacheditem/ToIsStoredCachedItemImplBaseImplTest.java
@@ -1,9 +1,13 @@
 package storedcacheditem.storedcacheditem;
 
+import com.google.protobuf.ByteString;
+import id.id.IsId;
+import id.output.IsOutput;
 import io.grpc.Deadline;
 import io.grpc.StatusRuntimeException;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import main.Test;
+import storedcacheditem.input.IsInput;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,12 +18,62 @@ public class ToIsStoredCachedItemImplBaseImplTest extends Test {
         final ToIsStoredCachedItemGrpc.ToIsStoredCachedItemBlockingStub toIsStoredCachedItemBlockingStub = ToIsStoredCachedItemGrpc.newBlockingStub(InProcessChannelBuilder.forName(ToIsStoredCachedItemGrpc.SERVICE_NAME)
                 .usePlaintext()
                 .build())
-                .withDeadline(Deadline.after(5,
+                .withDeadline(Deadline.after(30,
                         TimeUnit.SECONDS))
                 .withWaitForReady();
 
         final IsStoredCachedItem ignored = toIsStoredCachedItemBlockingStub.produce(NotStoredCachedItem.newBuilder()
                 .build());
+
+    }
+
+    @org.testng.annotations.Test(expectedExceptions = StatusRuntimeException.class, expectedExceptionsMessageRegExp = "INVALID_ARGUMENT: 422")
+    public void testStoreEmptyBytes() {
+        final ToIsStoredCachedItemGrpc.ToIsStoredCachedItemBlockingStub toIsStoredCachedItemBlockingStub = ToIsStoredCachedItemGrpc.newBlockingStub(InProcessChannelBuilder.forName(ToIsStoredCachedItemGrpc.SERVICE_NAME)
+                .usePlaintext()
+                .build())
+                .withDeadline(Deadline.after(30,
+                        TimeUnit.SECONDS))
+                .withWaitForReady();
+
+        final IsStoredCachedItem isStoredCachedItem = toIsStoredCachedItemBlockingStub.produce(NotStoredCachedItem.newBuilder()
+                .setIsInput(IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder()
+                                        .setIsStringValue(String.valueOf(System.currentTimeMillis()))
+                                        .build())
+                                .build())
+                        .setIsItemBytes(ByteString.copyFromUtf8(""))
+                        .setIsUseLockBoolean(true)
+                        .build())
+                .build());
+
+        assert isStoredCachedItem.hasIsOutput();
+
+    }
+
+    @org.testng.annotations.Test()
+    public void testStoreAccepted() {
+        final ToIsStoredCachedItemGrpc.ToIsStoredCachedItemBlockingStub toIsStoredCachedItemBlockingStub = ToIsStoredCachedItemGrpc.newBlockingStub(InProcessChannelBuilder.forName(ToIsStoredCachedItemGrpc.SERVICE_NAME)
+                .usePlaintext()
+                .build())
+                .withDeadline(Deadline.after(30,
+                        TimeUnit.SECONDS))
+                .withWaitForReady();
+
+        final IsStoredCachedItem isStoredCachedItem = toIsStoredCachedItemBlockingStub.produce(NotStoredCachedItem.newBuilder()
+                .setIsInput(IsInput.newBuilder()
+                        .setIsId(IsId.newBuilder()
+                                .setIsOutput(IsOutput.newBuilder()
+                                        .setIsStringValue(String.valueOf(System.currentTimeMillis()))
+                                        .build())
+                                .build())
+                        .setIsItemBytes(ByteString.copyFromUtf8("Hello " + System.currentTimeMillis()))
+                        .setIsUseLockBoolean(true)
+                        .build())
+                .build());
+
+        assert isStoredCachedItem.hasIsOutput();
 
     }
 }
